a54b7990b87271f90026f17c27eb3e0a4f41e623
MariemMimouni <mariemmimouni800@gmail.com>
2022-12-04 05:49:56 -0800

    0

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
-#!/user/bin/python3
-for c in [a..b]:
-    print(c, end='')
+#!/usr/bin/python3
+for letter in range(97, 123):
+        print("{}".format(chr(letter)), end="")
---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/user/bin/python3
+from add_0.py import add
+a = 1
+b = 1
+print(f"{a} + {b} = {add(a ,b)}")
