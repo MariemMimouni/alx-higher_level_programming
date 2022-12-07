3b041f0efacaa2626da4263624bce0135ba8f9a9
MariemMimouni <mariemmimouni800@gmail.com>
2022-12-07 13:13:15 -0800

    100T

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,6 +4,8 @@ def weight_average(my_list=[]):
         return 0
     else:
         s = 0
+        p = 0
         for i in my_list:
             s += i[0] * i[1]
-        return s
+            p += i[1]
+        return s/p
