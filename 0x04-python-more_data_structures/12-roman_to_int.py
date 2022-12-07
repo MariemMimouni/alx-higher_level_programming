269757e570e231cc506176a2bc23ddeabfdf7ed5
MariemMimouni <mariemmimouni800@gmail.com>
2022-12-07 14:27:56 -0800

    12T

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    else:
+        Rs = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        L = list()
+        for i in roman_string:
+            L.append(Rs[i])
+        n = L[0]
+        for j in range(1, len(L)):
+            if L[j] > L[j-1]:
+                n -= L[j-1]
+                n += L[j] - L[j-1]
+            else:
+                n += L[j]
+        return n
