56d8e4a27efbd4ab517490527179fe3ed42f5786
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-20 04:44:40 -0800

    2T

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if x == 0:
+        print()
+        return 0
+    try:
+        p = 0
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]))
+                p += 1
+            except TypeError:
+                pass
+            except ValueError:
+                pass
+    except IndexError:
+        pass
+    print()
+    return p
