b308da7493a62fb445194f87e8ab9c213483676d
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-20 04:12:51 -0800

    T0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
     except IndexError:
+        i -= 1
         pass
     print()
     return i + 1
