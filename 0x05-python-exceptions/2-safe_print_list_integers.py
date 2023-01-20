44b738870e75354d2dee83b6de9b9268c95aad3f
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-20 04:48:59 -0800

    2T

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         p = 0
         for i in range(x):
             try:
-                print("{:d}".format(my_list[i]))
+                print("{:d}".format(my_list[i]), end = "")
                 p += 1
             except TypeError:
                 pass
