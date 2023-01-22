3ccdfdbd2176702d09a7eac60e45dbb15923be38
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-22 05:59:10 -0800

    t1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """ Initialize a square
+        args: size """
+        self.__size = size
