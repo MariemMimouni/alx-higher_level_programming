e982d3c03f9baf58af311041186470db68aa01ef
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-22 06:09:37 -0800

    t2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a square."""
+
+
+class Square:
+    """ Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a square
+        args: size = 0"""
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not (type(size) == int):
+            raise TypeError("size must be an integer")
