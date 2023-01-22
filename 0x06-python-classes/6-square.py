87fe1e7de0e5a03666a51121e5a9d4f4966109a3
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-22 10:54:05 -0800

    t0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,10 +31,11 @@ class Square:
                 and len(value) == 2 
                 and isinstance(value[0], int)
                 and isinstance (value[1], int)
-                and value[0] > 0 
-                and value[1] > 0):
+                and value[0] >= 0 
+                and value[1] >= 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        elif :
+            self.__position = value
     
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a square
