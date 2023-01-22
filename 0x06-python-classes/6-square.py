3893fd46be2f4aa12a44eadfb7cb25679d7c0381
MariemMimouni <34914074+MariemMimouni@users.noreply.github.com>
2023-01-22 19:44:03 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,18 +4,7 @@
 
 class Square:
     """ Represent a square."""
-    def __init__(self, size=0, position=(0, 0)):
-        """Initialize a square
-        args: size = 0
-        position =(0, 0)"""
-        self.size = size
-        self.position = position
-
-    def area(self):
-        """Returns square area
-        args: self"""
-        return self.__size*self.__size
-
+    
     @property
     def size(self):
         """ Retrieve __size"""
@@ -29,20 +18,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def my_print(self):
-        """Prints the square in #"""
-        if self.__size == 0:
-            print()
-        else:
-            for y in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
 
     @property
     def position(self):
@@ -60,3 +35,29 @@ class Square:
                 and value[1] > 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+    
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a square
+        args: size = 0
+        position =(0, 0)"""
+        self.size = size
+        self.position = position
+    
+    def my_print(self):
+        """Prints the square in #"""
+        if self.__size == 0:
+            print()
+        else:
+            for y in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+        
+     def area(self):
+        """Returns square area
+        args: self"""
+        return self.__size*self.__size
