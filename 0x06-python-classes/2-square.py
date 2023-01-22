2e89643e6f8614e4788c3ce31431164af7b5ba45
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-22 06:11:47 -0800

    t2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,3 +11,4 @@ class Square:
             raise ValueError("size must be >= 0")
         if not (type(size) == int):
             raise TypeError("size must be an integer")
+        self.__size = size
