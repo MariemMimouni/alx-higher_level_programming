4a167a69dd40c8c793c29d763a1fc1046a4c2720
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-22 07:14:57 -0800

    t6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,8 @@ class Square:
     """ Represent a square."""
     def __init__(self, size=0, position(0, 0)):
         """Initialize a square
-        args: size = 0"""
+        args: size = 0
+        position =(0, 0)"""
         if not (isinstance(size, int)):
             raise TypeError("size must be an integer")
         elif size < 0:
