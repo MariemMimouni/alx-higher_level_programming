9b8dcdf74c09cc5fd818e2926f18d15ef8570d5c
MariemMimouni <mariemmimouni800@gmail.com>
2023-01-22 06:51:08 -0800

    t5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a square."""
+
+
+class Square:
+    """ Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a square
+        args: size = 0"""
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns square area
+        args: self"""
+        return self.__size*self.__size
+
+    @property
+    def size(self):
+        """ Retrieve __size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def my_print(self):
+        """Prints the square in #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
